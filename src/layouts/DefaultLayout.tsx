95ea990e65f8df3ecc790d7e69423bc5bc99f5cb
Naza <a290914469@noreply.gitcode.com>
2024-10-31 19:09:01 +0800

    update:添加联系方式

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,29 +1,36 @@
 import React, { PropsWithChildren } from 'react';
 import { Button, Layout, Tag, Flex, Input } from 'antd';
 const { Header, Content, Sider } = Layout;
-import { SettingOutlined, OpenAIOutlined } from '@ant-design/icons';
+import { SettingOutlined, OpenAIOutlined, GithubOutlined, WechatOutlined, DiscordOutlined } from '@ant-design/icons';
 import Logo from '@/assets/logo.svg';
 
 const DefaultLayout: React.FC<PropsWithChildren<{}>> = ({children}) => {
     return (
         <Layout style={{height: '100vh'}}>
             <Sider width={240} style={{background: '#fff', padding: 0, borderRight: '1px solid #DDD'}}>
-                <div className="mx-3 flex items-center h-16 text-2xl font-bold text-center mb-2">
-                    <Logo fill="#F73048" className="w-12" />&ensp;星群@CHAT
-                </div>
-                <Flex className="mx-3 space-y-3" vertical={true}>
-                    <Input.Search placeholder="搜索对话..."></Input.Search>
-                    <Button style={{ width: '100%' }} type="primary" size="large">+&ensp;创建新会话</Button>
+                <Flex className="h-full" vertical={true}>
+                    <div className="mx-3 flex items-center h-16 text-2xl font-bold text-center mb-2">
+                        <Logo fill="#F73048" className="w-12"/>&ensp;星群@CHAT
+                    </div>
+                    <Flex className="mx-3 flex-1 space-y-3" vertical={true}>
+                        <Input.Search placeholder="搜索对话..."></Input.Search>
+                        <Button style={{width: '100%'}} type="primary" size="large">+&ensp;创建新会话</Button>
+                    </Flex>
+                    <Flex className="mx-3 mb-3 space-x-4" justify="center" align="center">
+                        <GithubOutlined className="text-xl" color="#eee" />
+                        <WechatOutlined className="text-xl" />
+                        <DiscordOutlined className="text-xl" />
+                    </Flex>
                 </Flex>
             </Sider>
             <Layout>
                 <Header className="flex items-center w-full !p-4 border-b bg-white">
                     <Flex className="w-full" align="center">
                         <div className="flex-1 flex items-center">
-                            <OpenAIOutlined style={{ fontSize: '28px' }} />
+                            <OpenAIOutlined style={{fontSize: '28px'}}/>
                             <span className="mx-2 !text-xl font-bold">随便聊聊</span>
                             <Tag>
-                                ChatGPT-4o
+                            ChatGPT-4o
                             </Tag>
                             <Tag>
                                 测试代码数据库
