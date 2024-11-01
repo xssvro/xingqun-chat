62788d448cec96c669dbd85323c6fa2ce696d33a
xssvro <xssvro@gmail.com>
2024-11-01 18:49:30 +0800

    test:测滚动效果
    Author:    xssvro <xssvro@gmail.com>
    Date:      Fri Nov 1 18:49:30 2024 +0800
    
    On branch main
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/pages/Home/components/Chat.tsx
    	modified:   src/pages/Home/index.tsx
    	modified:   tailwind.config.js
    
    Changes not staged for commit:
    	modified:   src/pages/Home/components/Chat.tsx
    	modified:   src/styles/App.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      scrollbars:  {
+        thin: {
+          width: '2px'
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    tailwindScrollbar
+  ],
 }
